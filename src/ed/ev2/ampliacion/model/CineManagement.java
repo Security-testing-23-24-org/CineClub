333dec1999f88b300babb06c7854e5daf9bfd1b4
Javier Alvarez <fjalvarezs01@educarex.es>
2024-04-17 10:17:18 +0200

    Vulnerability I

--- src/ed/ev2/ampliacion/model/CineManagement.java
+++ src/ed/ev2/ampliacion/model/CineManagement.java
@@ -23,97 +23,116 @@ public class CineManagement {
 	private String dataBaseUser;
 	private String dataBasePass;
 	private String scriptCreacionDB;
-	private Connection conn;  // The connection must be the same for each test-case of the class.
-	
-	public static final int FILAS=10;
-	public static final int ASIENTOS=15;
-	private static final String CINECLUB_DB_H2_SCRIPT_CREACION_DB="cineclub.db.h2.script-creacion-db";
-	private static final String CINECLUB_DB_H2_DRIVER="cineclub.db.h2.driver";
-	private static final String CINECLUB_DB_H2_URL="cineclub.db.h2.url";
-	private static final String CINECLUB_DB_H2_USER="cineclub.db.h2.user";
-	private static final String CINECLUB_DB_H2_PASSWORD="cineclub.db.h2.password";
-	private static final String CINECLUB_DB_H2_CREACION_TABLA_OCUPACION="cineclub.db.h2.creacion-tabla-ocupacion";
-	private static final String CINECLUB_DB_H2_MODIFICAR_OCUPACION_BUTACA="cineclub.db.h2.modificar-ocupacion-butaca";
-	private static final String CINECLUB_DB_H2_OBTENER_OCUPACION_BUTACA="cineclub.db.h2.obtener-ocupacion-butaca";
-	private static final String CINECLUB_DB_H2_OBTENER_CLIENTE="cineclub.db.h2.obtener-cliente";
-	
-	public CineManagement() throws CineException{
+	private Connection conn; // The connection must be the same for each test-case of the class.
+
+	public static final int FILAS = 10;
+	public static final int ASIENTOS = 15;
+	private static final String CINECLUB_DB_H2_SCRIPT_CREACION_DB = "cineclub.db.h2.script-creacion-db";
+	private static final String CINECLUB_DB_H2_DRIVER = "cineclub.db.h2.driver";
+	private static final String CINECLUB_DB_H2_URL = "cineclub.db.h2.url";
+	private static final String CINECLUB_DB_H2_USER = "cineclub.db.h2.user";
+	private static final String CINECLUB_DB_H2_PASSWORD = "cineclub.db.h2.password";
+	private static final String CINECLUB_DB_H2_CREACION_TABLA_OCUPACION = "cineclub.db.h2.creacion-tabla-ocupacion";
+	private static final String CINECLUB_DB_H2_MODIFICAR_OCUPACION_BUTACA = "cineclub.db.h2.modificar-ocupacion-butaca";
+	private static final String CINECLUB_DB_H2_OBTENER_OCUPACION_BUTACA = "cineclub.db.h2.obtener-ocupacion-butaca";
+	private static final String CINECLUB_DB_H2_OBTENER_CLIENTE = "cineclub.db.h2.obtener-cliente";
+
+	public CineManagement() throws CineException {
 		try {
-			FileInputStream fileProperties=new FileInputStream("./Resources/Application.properties");
-			prop=new Properties();
+			FileInputStream fileProperties = new FileInputStream("./Resources/Application.properties");
+			prop = new Properties();
 			prop.load(fileProperties);
-			
-			dataBaseDriver=prop.getProperty(CINECLUB_DB_H2_DRIVER);
-			dataBaseURL=prop.getProperty(CINECLUB_DB_H2_URL);
-			dataBaseUser=prop.getProperty(CINECLUB_DB_H2_USER);
-			dataBasePass=prop.getProperty(CINECLUB_DB_H2_PASSWORD);
-			scriptCreacionDB=prop.getProperty(CINECLUB_DB_H2_SCRIPT_CREACION_DB);
-			
+
+			dataBaseDriver = prop.getProperty(CINECLUB_DB_H2_DRIVER);
+			dataBaseURL = prop.getProperty(CINECLUB_DB_H2_URL);
+			dataBaseUser = prop.getProperty(CINECLUB_DB_H2_USER);
+			dataBasePass = prop.getProperty(CINECLUB_DB_H2_PASSWORD);
+			scriptCreacionDB = prop.getProperty(CINECLUB_DB_H2_SCRIPT_CREACION_DB);
+
 			// DB Creation by means of a script execution
 			Class.forName(dataBaseDriver);
-			conn=DriverManager.getConnection(dataBaseURL,dataBaseUser,dataBasePass);
-			RunScript.execute(conn, new FileReader(scriptCreacionDB));			
-		} catch (SQLException | IOException | ClassNotFoundException e ) {
+			conn = DriverManager.getConnection(dataBaseURL, dataBaseUser, dataBasePass);
+			RunScript.execute(conn, new FileReader(scriptCreacionDB));
+		} catch (SQLException | IOException | ClassNotFoundException e) {
 			throw new CineException("ERROR en la generación de la base de datos!!!\n" + e.getMessage());
 		}
 
 //		conn.close();
 	}
-	
+
 	public boolean reservarButaca(ButacaVo b) {
-		boolean valorRetorno=false;
+		boolean valorRetorno = false;
 
 		try {
-			String queryModificarOcupacionButaca=prop.getProperty(CINECLUB_DB_H2_MODIFICAR_OCUPACION_BUTACA);
+			String queryModificarOcupacionButaca = prop.getProperty(CINECLUB_DB_H2_MODIFICAR_OCUPACION_BUTACA);
 			String queryParametrizada = SqlUtilidades.prepararQuery(queryModificarOcupacionButaca, new String[] {
-					Boolean.toString(b.isOcupado()), Integer.toString(b.getFila()), Integer.toString(b.getAsiento()) });			
-			Statement stmt=conn.createStatement();			
-			stmt.executeUpdate(queryParametrizada);		
-			valorRetorno=true;
+					Boolean.toString(b.isOcupado()), Integer.toString(b.getFila()), Integer.toString(b.getAsiento()) });
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(queryParametrizada);
+			valorRetorno = true;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return valorRetorno;
 	}
-	
-	public boolean verificarCredencialesCliente(String nombre, String password) {
-		boolean valorRetorno=false;
-		String nombreLeido="";
-		String passLeida="";
-		
-		try {
+
+//	public boolean verificarCredencialesCliente(String nombre, String password) {
+//		boolean valorRetorno=false;
+//		String nombreLeido="";
+//		String passLeida="";
+//		
+//		try {
 //			String queryObtenerCliente=prop.getProperty(CINECLUB_DB_H2_OBTENER_CLIENTE);
 //			String queryParametrizada = SqlUtilidades.prepararQuery(queryObtenerCliente, new String[] {
-//			nombre, password });
-			String queryObtenerCliente="SELECT * FROM cliente WHERE nombre = '" + nombre + "' AND password = '" + password + "'";
-					
-			Statement stmt=conn.createStatement();
-			ResultSet rs=stmt.executeQuery(queryObtenerCliente);
+//			nombre, password });					
+//			Statement stmt=conn.createStatement();
+//			ResultSet rs=stmt.executeQuery(queryParametrizada);
+//			while (rs.next()) {
+//				nombreLeido=rs.getString(1);
+//				passLeida=rs.getString(2);
+//			}
+//			if (nombre.equals(nombreLeido) && password.equals(passLeida)) {
+//				valorRetorno=true;
+//			}			
+//		} catch(SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		return valorRetorno;
+//	}
+
+	public boolean verificarCredencialesCliente(String nombre, String password) throws SQLException {
+		boolean valorRetorno = false;
+		String nombreLeido = "";
+		String passLeida = "";
+
+		String query = "SELECT * FROM cliente WHERE nombre = '" + nombre + "' AND pass = '" + password + "'";
+
+		try (Statement statement = conn.createStatement()) {
+
+			ResultSet rs = statement.executeQuery(query);
+
 			while (rs.next()) {
-				nombreLeido=rs.getString(2);
-				passLeida=rs.getString(4);
+				nombreLeido = rs.getString(1);
+				passLeida = rs.getString(2);
 			}
 			if (nombre.equals(nombreLeido) && password.equals(passLeida)) {
-				valorRetorno=true;
-			}			
-		} catch(SQLException e) {
-			e.printStackTrace();
+				valorRetorno = true;
+			}
 		}
-		
+
 		return valorRetorno;
 	}
-	
-	
-	private String prepararQuery(String q, String[] parametros) {		
-		StringBuilder sb=new StringBuilder(q);
-		
-		for (String parametroActual: parametros) {
-			sb.replace(sb.indexOf("?"), sb.indexOf("?")+1, parametroActual);
+
+	private String prepararQuery(String q, String[] parametros) {
+		StringBuilder sb = new StringBuilder(q);
+
+		for (String parametroActual : parametros) {
+			sb.replace(sb.indexOf("?"), sb.indexOf("?") + 1, parametroActual);
 		}
-		
+
 		return sb.toString();
 	}
-			
-	
+
 }
